41d91c7903e329fce13c983bda3b26c75f558bb6
Clara Bausson <clara.bausson@gmail.com>
2019-11-06 00:02:13 +0000

    Initial js work (added queue function, started chart drawing)

--- static/js/drawGraph.js
+++ static/js/drawGraph.js
@@ -1,4 +1,28 @@
 queue()
     .defer(d3.csv, "data/budget.csv")
     .await(drawGraphs);
+
+function drawGraphs(error, budgetData) {
+    var ndx = crossfilter(budgetData);
     
+    show_type_balance(ndx);
+    
+    dc.renderAll();
+}
+
+function show_type_balance(ndx) {
+    var dim = ndx.dimension(dc.pluck('category'));
+    var group = dim.group();
+    
+    dc.barChart("#debit_credit_balance")
+        .width(800)
+        .height(300)
+        .margins({top: 10, right: 50, bottom: 30, left: 50})
+        .dimension(dim)
+        .group(group)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .elasticY(false)
+        .xAxisLabel("Type");
+}
