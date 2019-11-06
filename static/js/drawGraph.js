bc0353e83520596261fecf86a6d94f8fb2cc99dd
Clara Bausson <clara.bausson@gmail.com>
2019-11-06 13:54:20 +0000

    Added initial piecharts

--- static/js/drawGraph.js
+++ static/js/drawGraph.js
@@ -4,25 +4,33 @@ queue()
 
 function drawGraphs(error, budgetData) {
     var ndx = crossfilter(budgetData);
-    
-    show_type_balance(ndx);
-    
+
+    show_spend_category_piechart(ndx);
+    show_income_category_piechart(ndx);
+
     dc.renderAll();
 }
 
-function show_type_balance(ndx) {
+function show_spend_category_piechart(ndx) {
     var dim = ndx.dimension(dc.pluck('category'));
-    var group = dim.group();
-    
-    dc.barChart("#debit_credit_balance")
-        .width(800)
-        .height(300)
-        .margins({top: 10, right: 50, bottom: 30, left: 50})
+    var group = dim.group().reduceSum(dc.pluck('debit amount'));
+
+    dc.pieChart('#debit_category_balance')
+        .height(200)
+        .radius(150)
+        .transitionDuration(1000)
         .dimension(dim)
-        .group(group)
-        .transitionDuration(500)
-        .x(d3.scale.ordinal())
-        .xUnits(dc.units.ordinal)
-        .elasticY(false)
-        .xAxisLabel("Type");
+        .group(group);
+}
+
+function show_income_category_piechart(ndx) {
+    var dim = ndx.dimension(dc.pluck('category'));
+    var group = dim.group().reduceSum(dc.pluck('credit amount'));
+
+    dc.pieChart('#credit_category_balance')
+        .height(200)
+        .radius(150)
+        .transitionDuration(1000)
+        .dimension(dim)
+        .group(group);
 }
